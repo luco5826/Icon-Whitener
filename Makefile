cf769bc38cc7999c0bf8011b847d0ba01017b7d6
Luca Errani <luca.errani1@gmail.com>
2019-01-28 19:18:24 +0100

    Small fixes

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
 	g++ whitener.cpp -lboost_filesystem -lboost_system -o white
+debug:
+	g++ -Wall -g whitener.cpp -lboost_filesystem -lboost_system -o white
--- whitener.cpp
+++ whitener.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <boost/filesystem.hpp>
@@ -5,9 +6,10 @@
 
 using namespace std;
 using namespace boost::filesystem;
+using namespace boost;
 
 int main()
-{   
+{
     cout << endl;
     cout << "ICON WHITENER By Luca Errani <luca.errani1@gmail.com>" << endl;
     cout << endl;
@@ -28,34 +30,38 @@ int main()
         if (is_regular_file(it->path()))
         {
             string fileName = it->path().string();
-            cout << fileName << endl;
-            std::ifstream fileIn;
-            std::ofstream fileOut;
-
-            // Opens the old svg and create a new one with a "w" char at the end (white)
-            fileIn.open(fileName);
-            fileOut.open(fileName + "w");
-
-            // For every line of the file, replaces (if exists)
-            // every occurrence of "fill:#000000" with "fill:#ffffff"
-            // which is from black to white
-            while (!fileIn.eof())
+            if (ends_with(fileName, ".svg"))
             {
-                string ss;
-                fileIn >> ss;
-            
-                boost::replace_all(ss, "fill:#000000", "fill:#ffffff");
-                
-                fileOut << ss;
-                fileOut << "\n";
-            }
-            fileIn.close();
-            fileOut.close();
 
-            // Deletes the old file
-            remove(fileName);
-            // Renames the new file as the old one
-            rename(fileName +  "w", fileName);
+                cout << fileName << endl;
+                std::ifstream fileIn;
+                std::ofstream fileOut;
+
+                // Opens the old svg and create a new one with a "w" char at the end (white)
+                fileIn.open(fileName);
+                fileOut.open(fileName + "w");
+
+                // For every line of the file, replaces (if exists)
+                // every occurrence of "fill:#000000" with "fill:#ffffff"
+                // which is from black to white
+                while (!fileIn.eof())
+                {
+                    string ss;
+                    fileIn >> ss;
+
+                    replace_all(ss, "fill:#000000", "fill:#ffffff");
+
+                    fileOut << ss;
+                    fileOut << "\n";
+                }
+                fileIn.close();
+                fileOut.close();
+
+                // Deletes the old file
+                remove(fileName);
+                // Renames the new file as the old one
+                rename(fileName + "w", fileName);
+            }
         }
     }
     cout << "FINISHED" << endl;
